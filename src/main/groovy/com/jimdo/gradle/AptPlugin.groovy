f5c7ed85f04322b5853c449c467f670a988de719
Stefano Dacchille <stefano.dacchille@gmail.com>
2013-10-31 12:16:52 +0100

    Fix indentation, sublime doesn't like groovy too much apparently

--- src/main/groovy/com/jimdo/gradle/AptPlugin.groovy
+++ src/main/groovy/com/jimdo/gradle/AptPlugin.groovy
@@ -11,81 +11,80 @@ class AptPlugin implements Plugin<Project> {
 
     if (isAndroidProject(project)) {
       applyToAndroidProject(project)
-      } else if (isJavaProject(project)) {
-        applyToJavaProject(project)
-        } else {
-          throw new IllegalArgumentException('The project misses the java, android or android-library plugin')
-        }
+    } else if (isJavaProject(project)) {
+      applyToJavaProject(project)
+    } else {
+        throw new IllegalArgumentException('The project misses the java, android or android-library plugin')
+    }
+  }
+
+  def applyToJavaProject(project) {
+    File aptOutputDir
+    if (!project.apt.outputDirName) {
+      aptOutputDir = project.file 'build/source/apt'
+    } else {
+      aptOutputDir = project.file outputDirName
+    }
+    project.task('addAptOptionsToCompileJava') << {
+      project.compileJava.options.compilerArgs.addAll '-processorpath',
+      project.configurations.apt.asPath, '-s', aptOutputDir.path
+
+      project.compileJava.source = project.compileJava.source.filter {
+        !it.path.startsWith(aptOutputDir.path)
       }
 
-      def applyToJavaProject(javaProject) {
-        File aptOutputDir
-        if (!javaProject.apt.outputDirName) {
-          aptOutputDir = javaProject.file 'build/source/apt'
-          } else {
-            aptOutputDir = javaProject.file outputDirName
-          }
+      project.compileJava.doFirst {
+        logger.info "Generating sources using the annotation processing tool:"
+        logger.info "  Output directory: ${aptOutputDir}"
 
-          javaProject.task('addAptOptionsToCompileJava') << {
-            javaProject.compileJava.options.compilerArgs.addAll '-processorpath',
-            javaProject.configurations.apt.asPath, '-s', aptOutputDir.path
+        aptOutputDir.mkdirs()
+      }
+    }
+    project.tasks.getByName('compileJava').dependsOn 'addAptOptionsToCompileJava'
+  }
 
-            javaProject.compileJava.source = javaProject.compileJava.source.filter {
-              !it.path.startsWith(aptOutputDir.path)
-            }
+  def applyToAndroidProject(androidProject) {
+    def androidExtension = androidProject.plugins.hasPlugin('android')? 
+    androidProject.plugins.getPlugin('android').extension : 
+    androidProject.plugins.getPlugin('android-library').extension
+    androidExtension.applicationVariants.all {
+    File aptOutputDir
+    if (!androidProject.apt.outputDirName) {
+      aptOutputDir = androidProject.file 'build/source/apt'
+      } else {
+        aptOutputDir = androidProject.file outputDirName
+      }
+      File variantAptOutputDir = androidProject.file("$aptOutputDir/$dirName")
 
-            javaProject.compileJava.doFirst {
-              logger.info "Generating sources using the annotation processing tool:"
-              logger.info "  Output directory: ${aptOutputDir}"
+      androidExtension.sourceSets[sourceSetName(it)].java.srcDirs.addAll variantAptOutputDir.path
 
-              aptOutputDir.mkdirs()
-            }
-          }
-          javaProject.tasks.getByName('compileJava').dependsOn 'addAptOptionsToCompileJava'
-        }
+      javaCompile.options.compilerArgs.addAll '-processorpath',
+      androidProject.configurations.apt.asPath, '-s', variantAptOutputDir.path
 
-        def applyToAndroidProject(androidProject) {
-          def androidExtension = androidProject.plugins.hasPlugin('android')? 
-          androidProject.plugins.getPlugin('android').extension : 
-          androidProject.plugins.getPlugin('android-library').extension
-          androidExtension.applicationVariants.all {
-            File aptOutputDir
-            if (!androidProject.apt.outputDirName) {
-              aptOutputDir = androidProject.file 'build/source/apt'
-              } else {
-                aptOutputDir = androidProject.file outputDirName
-              }
-              File variantAptOutputDir = androidProject.file("$aptOutputDir/$dirName")
+      javaCompile.source = javaCompile.source.filter {
+        !it.path.startsWith(aptOutputDir.path)
+      }
 
-              androidExtension.sourceSets[sourceSetName(it)].java.srcDirs.addAll variantAptOutputDir.path
+      def variant = it
+      javaCompile.doFirst {
+        logger.info "Generating sources using the annotation processing tool:"
+        logger.info "  Variant: ${variant.name}"
+        logger.info "  Output directory: ${variantAptOutputDir}"
 
-              javaCompile.options.compilerArgs.addAll '-processorpath',
-              androidProject.configurations.apt.asPath, '-s', variantAptOutputDir.path
+        variantAptOutputDir.mkdirs()
+      }
+    }
+  }
 
-              javaCompile.source = javaCompile.source.filter {
-                !it.path.startsWith(aptOutputDir.path)
-              }
+  def isAndroidProject(project) {
+    project.plugins.hasPlugin('android') || project.plugins.hasPlugin('android-library')
+  }
 
-              def variant = it
-              javaCompile.doFirst {
-                logger.info "Generating sources using the annotation processing tool:"
-                logger.info "  Variant: ${variant.name}"
-                logger.info "  Output directory: ${variantAptOutputDir}"
+  def isJavaProject(project) {
+    project.plugins.hasPlugin('java')
+  }
 
-                variantAptOutputDir.mkdirs()
-              }
-            }
-          }
-
-          def isAndroidProject(project) {
-            project.plugins.hasPlugin('android') || project.plugins.hasPlugin('android-library')
-          }
-
-          def isJavaProject(project) {
-            project.plugins.hasPlugin('java')
-          }
-
-          def sourceSetName(variant) {
-            variant.dirName.split('/').last()
-          }
-        }
+  def sourceSetName(variant) {
+    variant.dirName.split('/').last()
+  }
+}
