0ae50fe9086571c5ffa49ec486fed150f4b1683b
Stefano Dacchille <stefano.dacchille@gmail.com>
2013-10-31 16:27:01 +0100

    Refactor spec

--- src/test/groovy/com/jimdo/gradle/AptPluginSpec.groovy
+++ src/test/groovy/com/jimdo/gradle/AptPluginSpec.groovy
@@ -1,27 +1,20 @@
 package com.jimdo.gradle
 
+import org.gradle.testfixtures.ProjectBuilder
 import org.gradle.api.Project
 import org.gradle.api.Plugin
-import org.gradle.api.artifacts.ConfigurationContainer
-import org.gradle.api.plugins.ExtensionContainer
-import org.gradle.api.plugins.PluginContainer
 import spock.lang.Specification
 
 class AptPluginSpec extends Specification {
 
-  def project = Mock(Project)
-  def configContainer = Mock(ConfigurationContainer)
-  def extensionContainer = Mock(ExtensionContainer)
-  def pluginContainer = Mock(PluginContainer)
-
   def "it should throw an illegal argument exception if project isn't java or android"() {
+    given:
+    def project = ProjectBuilder.builder().build()
+
     when:
-    new AptPlugin().apply(project)
+    project.apply plugin: 'apt'
 
     then:
-    _ * project.configurations >> configContainer
-    _ * project.extensions >> extensionContainer
-    _ * project.plugins >> pluginContainer
     thrown(IllegalArgumentException)
   }
 }
