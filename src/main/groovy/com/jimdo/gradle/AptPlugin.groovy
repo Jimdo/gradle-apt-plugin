b6032fa014e0656eaee4aa4d003e4fa13f5146a1
Stefano Dacchille <stefano.dacchille@gmail.com>
2013-11-11 21:31:17 +0100

    Fix #1

--- src/main/groovy/com/jimdo/gradle/AptPlugin.groovy
+++ src/main/groovy/com/jimdo/gradle/AptPlugin.groovy
@@ -9,13 +9,14 @@ class AptPlugin implements Plugin<Project> {
     project.configurations.create 'apt'
     project.extensions.create 'apt', AptPluginExtension
 
-    // TODO: this doesn't work if the plugin is applied after the java/android one. How to fix it?
-    if (isAndroidProject(project)) {
-      applyToAndroidProject(project)
-    } else if (isJavaProject(project)) {
-      applyToJavaProject(project)
-    } else {
-        throw new IllegalArgumentException('The project misses the java, android or android-library plugin')
+    project.afterEvaluate {
+      if (isJavaProject(project)) {
+        applyToJavaProject(project)
+      } else if (isAndroidProject(project)) {
+        applyToAndroidProject(project)
+      } else  {
+          throw new IllegalArgumentException('The project misses the java, android or android-library plugin')
+      }
     }
   }
 
