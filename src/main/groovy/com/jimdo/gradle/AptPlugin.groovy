130eccfa70d12b25a025f17adc05c0d9dfaba688
Stefano Dacchille <stefano.dacchille@gmail.com>
2014-03-23 16:30:19 +0100

    Add support for android plugin 0.9.0 and 0.9.1

--- src/main/groovy/com/jimdo/gradle/AptPlugin.groovy
+++ src/main/groovy/com/jimdo/gradle/AptPlugin.groovy
@@ -8,7 +8,8 @@ import org.gradle.tooling.BuildException
 class AptPlugin implements Plugin<Project> {
 
   private static final String[] GRADLE_ANDROID_PLUGIN_SUPPORTED_VERSIONS = [ '0.7.0', '0.7.1', '0.7.2', '0.7.3', 
-                                                                             '0.8.0', '0.8.1', '0.8.2', '0.8.3' ]
+                                                                             '0.8.0', '0.8.1', '0.8.2', '0.8.3',
+                                                                             '0.9.0', '0.9.1']
 
   @Override void apply(Project project) {
     project.configurations.create 'apt'
