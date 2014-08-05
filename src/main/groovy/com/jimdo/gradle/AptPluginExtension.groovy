f4476fdb636f89efd53cec373c27491fa0e116dc
Yanus Poluektovich <ypoluektovich@gmail.com>
2014-08-06 00:45:04 +0400

    Handle all JavaCompile tasks
    In case of Java project, find and APT-ify all JavaCompile tasks,
    not just compileJava. Try to auto-guess good names for the
    configurations, extensions, helper tasks and output directories.
    
    No new special handling for Android projects.
    
    This commit also changes the default location of generated sources
    (for both Java and Android projects).

--- src/main/groovy/com/jimdo/gradle/AptPluginExtension.groovy
+++ src/main/groovy/com/jimdo/gradle/AptPluginExtension.groovy
@@ -2,4 +2,7 @@ package com.jimdo.gradle
 
 class AptPluginExtension {
   String outputDirName
+  AptPluginExtension(File projectBuildDir, String confName) {
+    outputDirName = [projectBuildDir, 'generated-sources', confName].join(File.separator)
+  }
 }
